431d320f0262a3461ddb9b970013884f075972a2
Lemon67a <kaanmtrx@gmail.com>
2025-07-26 22:10:53 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@ if [ ! -f velocity.jar ]; then
   wget https://api.papermc.io/v2/projects/velocity/versions/3.2.0/builds/315/downloads/velocity-3.2.0-SNAPSHOT-315.jar -O velocity.jar
 fi
 java -Xms64M -Xmx128M -jar velocity.jar
+#BUNU SİLMEYİN!!!
